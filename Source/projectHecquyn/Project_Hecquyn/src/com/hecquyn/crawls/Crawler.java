2ebe7954cfeaa431733dc6742c435894ca158eae
trilam1409 <lamviettri1996@gmail.com>
2017-03-20 13:32:05 +0700

    Add: Updata Crawler
    Tạo kế thừa và crawler từng trang bằng từng class tương ứng

---
+++ Source/projectHecquyn/Project_Hecquyn/src/com/hecquyn/crawls/Crawler.java
@@ -0,0 +1,20 @@
+package com.hecquyn.crawls;
+
+import java.util.ArrayList;
+
+import javax.xml.crypto.Data;
+
+public class Crawler {
+
+    public String url;
+
+    public Crawler(String link) {
+        url = link;
+    }
+    
+    public ArrayList<com.hecquyn.database.Data>  processPage(){
+		return null;};
+        
+        
+
+}
