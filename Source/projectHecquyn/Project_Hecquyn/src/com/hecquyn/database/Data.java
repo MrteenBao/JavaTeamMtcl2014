178da720b50aad5dfefca08b883a9efadb3e0b85
Bao <bumkute1320@gmail.com>
2017-04-09 16:15:29 +0700

    Update Data

--- Source/projectHecquyn/Project_Hecquyn/src/com/hecquyn/database/Data.java
+++ Source/projectHecquyn/Project_Hecquyn/src/com/hecquyn/database/Data.java
@@ -10,19 +10,38 @@ package com.hecquyn.database;
  * @author Vuong Gia Phu
  */
 public class Data {
-    private String link;
-    private String text;
-    
-    public Data(String link, String text) {
-        this.link = link;
-        this.text = text;
-    }
-    
-    public String getLink() {
-        return link;
-    }
-    
-    public String getText() {
-        return text;
-    }
+	private String _Link;
+	private String _Title;
+	private String _Image;
+	private String _Author;
+	private String _Date;
+	private String _Hashtag;
+	public Data(String _Link,String _Title,String _Image, String _Author, String _Date, String _Hashtag)
+	{
+		this._Link=_Link;
+		this._Title=_Title;
+		this._Image=_Image;
+		this._Author=_Author;
+		this._Date=_Date;
+		this._Hashtag=_Hashtag;
+	}
+	public String Set_Link(){
+		return _Link;
+	}
+	public String Set_Title(){
+		return _Title;
+	}
+	public String Set_Image(){
+		return _Image;
+	}
+	public String Set_Author(){
+		return _Author;
+	}
+	public String Set_Date(){
+		return _Date;
+	}
+	public String Set_Hashtag(){
+		return _Hashtag;
+	}
+	
 }
