9da46a6054d274fa24a25e8ab507179153243766
phuvg <phuvg1996@gmail.com>
2017-04-14 06:12:32 +0700

    update vnexpress

--- Source/projectHecquyn/Project_Hecquyn/src/com/hecquyn/crawl/Vnexpress.java
+++ Source/projectHecquyn/Project_Hecquyn/src/com/hecquyn/crawl/Vnexpress.java
@@ -22,11 +22,14 @@ public class Vnexpress extends Crawler {
     public Vnexpress(String link) {
         super(link);
     }
-  
+
+    @Override
     public ArrayList<Data> processPage() {
         ArrayList<Data> list = new ArrayList<Data>();
         String link = "";
-        String text = "";
+        String txt = "";
+        ArrayList<String> text = new ArrayList<String>();
+        ArrayList<String> tag = new ArrayList<String>();
         try {
             Document doc = Jsoup.connect(url).get();
             Elements elements = doc.getElementsByTag("h3");
@@ -38,11 +41,36 @@ public class Vnexpress extends Crawler {
                         Document doc1 = Jsoup.connect(link).get();
                         Elements elements1 = doc1.getElementsByTag("p");
                         for (Element e1 : elements1) {
-                            text = text + e1.attr("class", "Normal").toString() + "\n";
+                            txt = txt + e1.attr("class", "Normal").toString() + "\n";
                         }
-                        Data data = new Data(link, text);
-                        list.add(data);
-                        text = "";
+                        text.add(txt);
+                        txt = "";
+                        
+                        Elements elements2 = doc1.getElementsByTag("div");
+                        for (Element _elements2 : elements2) {
+                            if (_elements2.hasClass("block_tag width_common space_bottom_20")) {
+                                Elements e2 = _elements2.getElementsByTag("a");
+                                for(Element _e2 : e2) {
+                                    Element e3 = _e2.attr("class", "tag_item");
+                                    String str = e3.attr("title").toString();
+                                    tag.add(str);
+                                    System.out.println(str);
+                                    System.out.println("--");
+                                }
+                                System.out.println("---------------");
+                                for(int i = 0; i<tag.size(); i++) {
+                                    System.out.println(tag.get(i));
+                                }
+                                System.out.println("=======================");
+                                int temp = 0;
+                                Data data = new Data(link, text.get(temp), tag);
+                                list.add(data);
+                                System.out.println(tag.size());
+                                for(int i = 0; i < tag.size(); i++) {
+                                    tag.remove(i);
+                                }
+                            }
+                        }
                     }
                 }
             }
