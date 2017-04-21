2afc6694f91e12ba83a16c049856d236dc8d5058
phuvg <phuvg1996@gmail.com>
2017-04-21 13:16:58 +0700

    update start

--- Source/projectHecquyn/Project_Hecquyn/src/com/hecquyn/myiu/Start.java
+++ Source/projectHecquyn/Project_Hecquyn/src/com/hecquyn/myiu/Start.java
@@ -8,12 +8,21 @@ package com.hecquyn.myiu;
 import com.hecquyn.crawl.*;
 import com.hecquyn.database.Data;
 import com.hecquyn.database.MyDatabase;
+import static com.hecquyn.database.MyDatabase.db;
+import static com.hecquyn.database.MyDatabase.getMongoClient;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.MongoClient;
 import java.io.IOException;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.swing.JButton;
-import javax.swing.JTextArea;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import org.bson.Document;
 
 /**
  *
@@ -28,16 +37,26 @@ public class Start extends javax.swing.JFrame {
         initComponents();
     }
 
-    
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jFrame1 = new javax.swing.JFrame();
         jLabel1 = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
         taInformation = new javax.swing.JTextArea();
         btnStart = new javax.swing.JButton();
-        lblNoti = new javax.swing.JLabel();
+
+        javax.swing.GroupLayout jFrame1Layout = new javax.swing.GroupLayout(jFrame1.getContentPane());
+        jFrame1.getContentPane().setLayout(jFrame1Layout);
+        jFrame1Layout.setHorizontalGroup(
+            jFrame1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 400, Short.MAX_VALUE)
+        );
+        jFrame1Layout.setVerticalGroup(
+            jFrame1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 300, Short.MAX_VALUE)
+        );
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -50,7 +69,7 @@ public class Start extends javax.swing.JFrame {
         taInformation.setEditable(false);
         taInformation.setColumns(20);
         taInformation.setRows(5);
-        taInformation.setText("Hecules project: crawl online newspageper and hashtag the news.\n- Đặng Thiên Bảo 14520047\n- Lâm Việt Trí 1452\n- Từ Vĩnh Nguyên 14520615\n- Vương Gia Phú 14520688 ");
+        taInformation.setText("Hecules project: crawl online newspageper and hashtag the news.\n- Đặng Thiên Bảo 14520047\n- Lâm Việt Trí 14520992\n- Từ Vĩnh Nguyên 14520615\n- Vương Gia Phú 14520688 ");
         taInformation.setName("taInformation_Start"); // NOI18N
         jScrollPane1.setViewportView(taInformation);
 
@@ -65,8 +84,6 @@ public class Start extends javax.swing.JFrame {
             }
         });
 
-        lblNoti.setFont(new java.awt.Font("Tahoma", 2, 14)); // NOI18N
-
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -74,14 +91,13 @@ public class Start extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(lblNoti, javax.swing.GroupLayout.PREFERRED_SIZE, 106, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(18, 18, 18)
-                        .addComponent(btnStart, javax.swing.GroupLayout.PREFERRED_SIZE, 131, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(0, 125, Short.MAX_VALUE))
                     .addComponent(jLabel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.TRAILING))
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 428, Short.MAX_VALUE))
                 .addContainerGap())
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(btnStart, javax.swing.GroupLayout.PREFERRED_SIZE, 131, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(155, 155, 155))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -91,59 +107,78 @@ public class Start extends javax.swing.JFrame {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 143, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(btnStart, javax.swing.GroupLayout.DEFAULT_SIZE, 56, Short.MAX_VALUE)
-                    .addComponent(lblNoti, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addComponent(btnStart, javax.swing.GroupLayout.DEFAULT_SIZE, 56, Short.MAX_VALUE)
+                .addContainerGap(13, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-    
-    public MyDatabase db = new MyDatabase();
-         
+
+    MyDatabase db;
+
     private void btnStartActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnStartActionPerformed
-        lblNoti.setText("Crawling...");
         try {
+            dbConnection();
             vnexpress();
-            zingnews();
-            bongdavn();
+            //zingnews();
+            //bongdavn();
         } catch (IOException ex) {
             Logger.getLogger(Start.class.getName()).log(Level.SEVERE, null, ex);
         }
-        lblNoti.setText("Done!");
+        JOptionPane.showMessageDialog(rootPane, "Done!");
     }//GEN-LAST:event_btnStartActionPerformed
-    
+
+    private static void dbConnection() throws UnknownHostException {
+        try {
+            MongoClient mongoClient = getMongoClient();
+            /*System.out.println("List all DB:");
+            List<String> dbNames = mongoClient.getDatabaseNames();
+            for (String dbName : dbNames) {
+                System.out.println("+ DB Name: " + dbName);
+            }
+
+            System.out.println("Done!");*/
+
+        } catch (IOException e) {
+            System.out.println("ERROR DATABASE CONNECTION!");
+        }
+    }
+
     public void vnexpress() throws IOException {
         String url = "http://vnexpress.net/";
+        db = new MyDatabase("vnexpress");
+        
         ArrayList<Data> list = new ArrayList<Data>();
         Vnexpress vnexpress = new Vnexpress(url);
         list = vnexpress.processPage();
-        for(int i = 0; i < list.size(); i++) {
-            db.insert(url, list.get(i));
+        for (int i = 0; i < list.size(); i++) {
+            db.Collection.insertOne(new Document("_Link", list.get(i).getLink())
+                                    .append("_Title", list.get(i).getTitle())
+                                    .append("_Date", list.get(i).getDate())
+                                    .append("_Hashtag", list.get(i).getTag()));
         }
     }
-    
+
     public void zingnews() throws IOException {
         String url = "http://news.zing.vn/";
         ArrayList<Data> list = new ArrayList<Data>();
         Crawler zingnews = new Zingnews(url);
         list = zingnews.processPage();
-        for(int i = 0; i < list.size(); i++) {
-            db.insert(url, list.get(i));
+        for (int i = 0; i < list.size(); i++) {
+            //db.insert(url, list.get(i));
         }
     }
-    
+
     public void bongdavn() throws IOException {
         String url = "http://bongda.vn/";
         ArrayList<Data> list = new ArrayList<Data>();
         Bongda bongda = new Bongda(url);
         list = bongda.processPage();
-        for(int i = 0; i < list.size(); i++) {
-            db.insert(url, list.get(i));
+        for (int i = 0; i < list.size(); i++) {
+            //db.insertOne(url, list.get(i));
         }
     }
-    
+
     /**
      * @param args the command line arguments
      */
@@ -181,9 +216,9 @@ public class Start extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnStart;
+    private javax.swing.JFrame jFrame1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JLabel lblNoti;
     private javax.swing.JTextArea taInformation;
     // End of variables declaration//GEN-END:variables
 }
